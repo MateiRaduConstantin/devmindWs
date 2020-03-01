eac0300dc936da43f1db67fc45f632189e9098bc
Oana <oana27029@gmail.com>
2020-03-01 13:44:46 +0200

    pick me

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
+import java.util.List;
+
 public class Test {
-    public Test() {
+    public Test(List<String> wishlist) {
         // this is the best comment ever
+        System.out.println("Pick me");
     }
 }
