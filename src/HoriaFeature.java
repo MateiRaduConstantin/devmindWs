c9768a85c370af8f83e9ed04d8f8d1e9e730eeb6
vlad <prioteasa.vlad@gmail.com>
2020-03-01 13:41:07 +0200

    Adevaraciune

--- src/HoriaFeature.java
+++ src/HoriaFeature.java
@@ -2,5 +2,12 @@ public class HoriaFeature {
     public static String bestJokeEver = "True";
 
     // this is a random class
-    
+
+    public static String getBestJokeEver() {
+        return bestJokeEver;
+    }
+
+    public static void setBestJokeEver(String bestJokeEver) {
+        HoriaFeature.bestJokeEver = bestJokeEver;
+    }
 }
