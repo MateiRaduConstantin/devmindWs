26aa3ce546c4b80304773be68f5b3546284a77a3
radu2324 <mateiraduconstantin@gmail.com>
2020-11-23 18:39:54 +0200

    mi-am facut tema

--- src/HoriaFeature.java
+++ src/HoriaFeature.java
@@ -2,6 +2,7 @@ public class HoriaFeature {
     public static String bestJokeEver = "True";
 
     // this is a random class
+    // modificare random
 
     public static String getBestJokeEver() {
         return bestJokeEver;
